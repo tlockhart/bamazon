1b130d776b1240e279b1c37c7dc399c349f84791
tlockhart <tony.lockhart@ymail.com>
2018-10-31 15:38:32 -0400

    Completed product display and update in customer.js file and updated initDB.sql

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,6 +4,7 @@
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 var Table = require('cli-table');
+var displayCtr = 0;
 
 /*****************************************************
  *  CONNECTION TO DATABASE
@@ -32,12 +33,15 @@ var connection = mysql.createConnection({
     //START PROGRAM
     /***************/
     //promptUser();
-    displayProducts()
+    displayProducts();
 
     /*****************
      * END CONNECTION
      *****************/
-    connection.end();
+    ///DEPENDENT ON USR RESPONSE:
+    //connection.end();
+    //process.exit(0);//0 means we are making a clean exit
+    /************************/
   });
 
   /*********************************************/
@@ -60,12 +64,114 @@ function displayProducts() {
         [results[i].item_id, results[i].product_name, results[i].price]
       );
     }
-
-    console.log(table.toString());
+    //If products table has not been displayed show it
+    if(displayCtr < 1){
+      console.log(table.toString());
+      displayCtr++;
+    }
+    
+    //promptUser(results);
+    var syntax = /^[A-Za-z]+$/;
+  inquirer.prompt([
+      {
+        name: "item",
+        message: "Enter an item ID to purchase?\n",
+        type: "number",
+        validate: function(item){
+          if(isNaN(item) === false && item > 0 && item < results.length){
+              return true;
+          }
+          else{
+            //console.log("Item ID not found, please try again.");
+              return false;
+          }//else
+        }//validate
+      },//prompt
+      {
+        name: "quantity",
+        message: "How many units would you like to purchase?\n",
+        type: "number",
+        validate: function(quantity){
+          //console.log("Item = "+item);
+          if(isNaN(quantity) === false && quantity >0){
+              return true;
+          }
+          else{
+            //console.log("Quantity not available in stock, please try again.");
+              return false;
+          }//else
+        }
+      }
+    ]).then(function(response) {
+      //Examines user input and sends a callback function in as an argument.
+        //console.log("Name = "+response.name.toLowerCase());
+        //processInput(response.name.toLowerCase(), promptUser);
+        testFunc(results, response);
+    });
+    //testFunc(results);
   });//query
 
 }//displayProducts
 
+//PROMPT USER FOR INPUT
+/*function promptUser(results){
+  
+}//promptUser*/
+
+function testFunc(results, response){
+  console.log("Item_ID = "+response.item);
+  console.log("Quantity = "+response.quantity);
+  //&& quantity <= results[item].stock_quantity
+  console.log("Available Quantity = "+results[response.item-1].stock_quantity);
+  console.log("Price = "+results[response.item-1].price);
+  console.log("Item Name = "+results[response.item-1].product_name);
+  var productName = results[response.item-1].product_name;
+  var price = results[response.item-1].price;
+  var userChoice = response.item;
+  var userQuantity = response.quantity;
+  var stockQuantity = results[response.item-1].stock_quantity;
+  var newQuantity;
+  var cost =0; 
+      if (userQuantity <= stockQuantity){
+        //Update Database stock_quantity
+        newQuantity = stockQuantity - userQuantity;
+        console.log("Update the quantity in products database.");
+        cost = price*userQuantity;
+        console.log(userQuantity +" "+productName+"'s at "+price+" = "+cost);
+        updateInventory(userChoice, newQuantity)
+        //figure out bill and output to user
+        //restart 
+      }
+      else
+      {
+        //Let user know not enough in stock
+        //Show products table and then loop back from start.
+        console.log("Quantity is not in stock.");
+        displayProducts();
+      }
+}//testFunc
+
+function updateInventory(item, newQuantity){
+  connection.query(
+    "UPDATE products SET ? WHERE ?",
+    [
+      {
+        stock_quantity: newQuantity
+      },
+      {
+        item_id: item
+      }
+    ],
+    function(error) {
+      if (error) throw error;
+
+      console.log("Inventory Updated successfully!");
+      //reset displayCtr after transaction is complete
+      displayCtr = 0;
+      //displayProducts();
+    }
+  );
+}//updateInventery
 /*****************************************************
  *  RUN PROGRAMS
  ****************************************************/
--- initDB.sql
+++ initDB.sql
@@ -6,35 +6,43 @@ CREATE TABLE products(
   item_id INTEGER(30) AUTO_INCREMENT NOT NULL,
   product_name VARCHAR(30) NOT NULL,
   department_name VARCHAR(30) NOT NULL,
-  price INTEGER(30) NOT NULL,
+  price DECIMAL(30, 2) NOT NULL,
   stock_quantity INTEGER(30) NOT NULL,
-  product_sales INTEGER(30) NULL,
+  product_sales DECIMAL(30, 2) NULL,
   PRIMARY KEY (item_id)
 );
 
 CREATE TABLE departments(
   department_id INTEGER(11) AUTO_INCREMENT NOT NULL,
   department_name VARCHAR(30) NOT NULL,
-  over_head_costs INTEGER(30) NOT NULL,
+  over_head_costs DECIMAL(30, 2) NOT NULL,
   PRIMARY KEY (department_id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ('hair dryer', 'Home & Kitchen', 50, 20);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ('hair brush', 'Home & Kitchen', 5, 20);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ('LG TV', 'Electronics', 1000, 10);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ('PS4 Red Dead Redemption', 'Toys & Games', 60, 30);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ('OWL Diaries Box Set', 'Books', 20, 10);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Lego City Advent Calender', 'Toys & Games', 20, 10);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Alexa FireStick Remote', 'Electronics', 40, 20);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ('NOCO Battery Charger', 'Automative', 30, 20);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ('L.O.L. Surprise! Glam Doll', 'Toys & Games', 10, 20);
-INSERT INTO products (product_name, department_name, price, stock_quantity) values ('Crayola LightUp Tracing Pad', 'Toys & Games', 25, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES 
+  ('hair dryer', 'Home & Kitchen', 50.00, 20), 
+  ('hair brush', 'Home & Kitchen', 5.00, 2), 
+  ('LG TV', 'Electronics', 1000.00, 10), 
+  ('PS4 Red Dead Redemption', 'Toys & Games', 60.00, 30),
+  ('OWL Diaries Box Set', 'Books', 20.00, 10),
+  ('Lego City Advent Calender', 'Toys & Games', 20.00, 10),
+  ('Alexa FireStick Remote', 'Electronics', 40.00, 20),
+  ('NOCO Battery Charger', 'Automative', 30.00, 20),
+  ('L.O.L. Surprise! Glam Doll', 'Toys & Games', 10.00, 20),
+  ('Crayola LightUp Tracing Pad', 'Toys & Games', 25.00, 20);
 
-INSERT INTO departments (department_name, over_head_costs) values ('Home & Kitchen', 500);
-INSERT INTO departments (department_name, over_head_costs) values ('Electronics', 10000);
-INSERT INTO departments (department_name, over_head_costs) values ('Toys & Games', 1000);
-INSERT INTO departments (department_name, over_head_costs) values ('Books', 800);
-INSERT INTO departments (department_name, over_head_costs) values ('Automotive', 500);
+
+
+/*INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('PS4 Red Dead Redemption', 'Toys & Games', 60, 30);*/
+
+
+INSERT INTO departments (department_name, over_head_costs) values ('Home & Kitchen', 500.00);
+INSERT INTO departments (department_name, over_head_costs) values ('Electronics', 10000.00);
+INSERT INTO departments (department_name, over_head_costs) values ('Toys & Games', 1000.00);
+INSERT INTO departments (department_name, over_head_costs) values ('Books', 800.00);
+INSERT INTO departments (department_name, over_head_costs) values ('Automotive', 500.00);
 
 -- execute 
 -- mysql -u root -p < initDB.sql 
